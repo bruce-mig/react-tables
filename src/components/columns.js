49c0a621e4ad1c85bff11113a16708a4f3186fe6
bruce-mig <brucetmig@icloud.com>
2025-01-24 13:51:19 +0200

    added header groups

--- src/components/columns.js
+++ src/components/columns.js
@@ -24,3 +24,40 @@ export const COLUMNS = [
         accessor: 'phone'
     }
 ]
+
+export const GROUPED_COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Name',
+        columns: [
+            {
+                Header: 'First Name',
+                accessor: 'first_name'
+            },
+            {
+                Header: 'Last Name',
+                accessor: 'last_name'
+            }
+        ]
+    },
+    {
+        Header: 'Info',
+        columns: [
+            {
+                Header: 'Date of Birth',
+                accessor: 'date_of_birth'
+            },
+            {
+                Header: 'Country',
+                accessor: 'country'
+            },
+            {
+                Header: 'Phone',
+                accessor: 'phone'
+            }
+        ]
+    }
+]
