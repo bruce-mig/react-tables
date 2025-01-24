4abc0b0119f5c4713ec80e4d27456c697f94edd5
bruce-mig <brucetmig@icloud.com>
2025-01-24 11:47:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BasicTable } from './components/BasicTable'
+
+function App() {
+  return (
+    <div className='App'>
+      <BasicTable />
+    </div>
+  )
+}
+
+export default App
