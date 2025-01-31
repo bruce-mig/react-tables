b826d9985d5ccc40cbca11775875892f66f8b6fe
bruce-mig <brucetmig@icloud.com>
2025-01-31 11:45:27 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import { FilteringTable } from './components/FilteringTable'
 import { PaginationTable } from './components/PaginationTable'
 
 function App() {
-  return (
-    <div className='App'>
-      <PaginationTable />
-    </div>
-  )
+   return (
+      <div className='App'>
+         <PaginationTable />
+      </div>
+   )
 }
 
 export default App
