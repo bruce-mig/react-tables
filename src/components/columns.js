c8836a609a924881ee33842b30d3a9ac38d422b2
bruce-mig <brucetmig@icloud.com>
2025-01-31 11:45:40 +0200

    columns

--- src/components/columns.js
+++ src/components/columns.js
@@ -1,71 +1,71 @@
 import { ColumnFilter } from "./ColumnFilter"
 
 export const COLUMNS = [
-    {
-        Header: 'Id',
-        accessor: 'id',
-        disableFilters: true
-    },
-    {
-        Header: 'First Name',
-        accessor: 'first_name',
-        disableFilters: true
-    },
-    {
-        Header: 'Last Name',
-        accessor: 'last_name',
-        disableFilters: true
-    },
-    {
-        Header: 'Date of Birth',
-        accessor: 'date_of_birth',
-        disableFilters: true
-    },
-    {
-        Header: 'Country',
-        accessor: 'country',
-        disableFilters: true
-    },
-    {
-        Header: 'Phone',
-        accessor: 'phone',
-        disableFilters: true
-    }
+   {
+      Header: 'Id',
+      accessor: 'id',
+      disableFilters: true
+   },
+   {
+      Header: 'First Name',
+      accessor: 'first_name',
+      disableFilters: true
+   },
+   {
+      Header: 'Last Name',
+      accessor: 'last_name',
+      disableFilters: true
+   },
+   {
+      Header: 'Date of Birth',
+      accessor: 'date_of_birth',
+      disableFilters: true
+   },
+   {
+      Header: 'Country',
+      accessor: 'country',
+      disableFilters: true
+   },
+   {
+      Header: 'Phone',
+      accessor: 'phone',
+      disableFilters: true
+   }
 ]
 
 export const GROUPED_COLUMNS = [
-    {
-        Header: 'Id',
-        accessor: 'id'
-    },
-    {
-        Header: 'Name',
-        columns: [
-            {
-                Header: 'First Name',
-                accessor: 'first_name'
-            },
-            {
-                Header: 'Last Name',
-                accessor: 'last_name'
-            }
-        ]
-    },
-    {
-        Header: 'Info',
-        columns: [
-            {
-                Header: 'Date of Birth',
-                accessor: 'date_of_birth'
-            },
-            {
-                Header: 'Country',
-                accessor: 'country'
-            },
-            {
-                Header: 'Phone',
-                accessor: 'phone'
-            }
-        ]
-    }
+   {
+      Header: 'Id',
+      accessor: 'id'
+   },
+   {
+      Header: 'Name',
+      columns: [
+         {
+            Header: 'First Name',
+            accessor: 'first_name'
+         },
+         {
+            Header: 'Last Name',
+            accessor: 'last_name'
+         }
+      ]
+   },
+   {
+      Header: 'Info',
+      columns: [
+         {
+            Header: 'Date of Birth',
+            accessor: 'date_of_birth'
+         },
+         {
+            Header: 'Country',
+            accessor: 'country'
+         },
+         {
+            Header: 'Phone',
+            accessor: 'phone'
+         }
+      ]
+   }
 ]
