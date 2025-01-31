0526c188830c72fedd2b22dd686ed66ba158d256
bruce-mig <brucetmig@icloud.com>
2025-01-31 11:46:27 +0200

    added paging table

--- src/components/PaginationTable.jsx
+++ src/components/PaginationTable.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo} from 'react'
+import React, { useMemo } from 'react'
 import { useTable, usePagination } from 'react-table'
 import MOCK_DATA from './MOCK_DATA.json'
 import { COLUMNS } from './columns'
@@ -6,71 +6,71 @@ import './table.css'
 
 export const PaginationTable = () => {
 
-    const columns = useMemo(() => COLUMNS, [])
-    const data = useMemo(() => MOCK_DATA, [])
-    
-    const { 
-        getTableProps, 
-        getTableBodyProps, 
-        headerGroups, 
-        page,
-        nextPage,
-        previousPage,
-        canNextPage,
-        canPreviousPage,
-        pageOptions,
-        state,
-        prepareRow 
-    } = useTable({
-        columns,
-        data
-    }, 
-    usePagination
-    )
+   const columns = useMemo(() => COLUMNS, [])
+   const data = useMemo(() => MOCK_DATA, [])
 
-    const { pageIndex } = state
-    
+   const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      page,
+      nextPage,
+      previousPage,
+      canNextPage,
+      canPreviousPage,
+      pageOptions,
+      state,
+      prepareRow
+   } = useTable({
+      columns,
+      data
+   },
+      usePagination
+   )
 
-  return (
-    <>
-    <table {...getTableProps()}>
-        <thead>
-            {headerGroups.map((headerGroup => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map((column) => (
+   const { pageIndex } = state
+
+
+   return (
+      <>
+         <table {...getTableProps()}>
+            <thead>
+               {headerGroups.map((headerGroup => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                     {headerGroup.headers.map((column) => (
                         <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                    ))}
-                </tr>
-            )))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-            {
-                page.map(row => {
-                    prepareRow(row)
-                    return (
+                     ))}
+                  </tr>
+               )))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+               {
+                  page.map(row => {
+                     prepareRow(row)
+                     return (
                         <tr {...row.getRowProps()}>
-                            {row.cells.map((cell) => {
-                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                            })}
+                           {row.cells.map((cell) => {
+                              return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                           })}
                         </tr>
-                    )
-                })
-            }
-            <tr>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
-    <div>
-        <span>
-            Page{' '}
-            <strong>
-                {pageIndex + 1} of {pageOptions.length}
-            </strong>{' '}
-        </span>
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
-        <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
-    </div>
-    </>
-  )
+                     )
+                  })
+               }
+               <tr>
+                  <td></td>
+               </tr>
+            </tbody>
+         </table>
+         <div>
+            <span>
+               Page{' '}
+               <strong>
+                  {pageIndex + 1} of {pageOptions.length}
+               </strong>{' '}
+            </span>
+            <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
+            <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+         </div>
+      </>
+   )
 }
