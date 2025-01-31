8aee84f38c95ed3ce7012c9e320df38276f93dc3
bruce-mig <brucetmig@icloud.com>
2025-01-31 10:30:51 +0200

    added global filtering

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BasicTable } from './components/BasicTable'
+import { SortingTable } from './components/SortingTable'
+import { FilteringTable } from './components/FilteringTable'
 
 function App() {
   return (
     <div className='App'>
-      <BasicTable />
+      <FilteringTable />
     </div>
   )
 }
